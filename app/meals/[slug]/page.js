e6b69a8aac3f28df937be0decf31c71aa0f0f006
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-06 03:05:07 +0530

    add imgs

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MealDetails = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default MealDetails
---
+++ app/meals/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Meals() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Meals
