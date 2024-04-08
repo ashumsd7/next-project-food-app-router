8c1806d67ce1bc5b3eb632e5a51d729ced70f045
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-09 02:22:07 +0530

    add dummy Image pciker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["media-assets.swiggy.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
